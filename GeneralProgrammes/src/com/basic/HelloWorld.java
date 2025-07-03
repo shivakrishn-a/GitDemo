2023f3471b4fe20d50aa694d435f2c654265d20f
shiva <bandewarshivakrishna@gmail.com>
2025-07-03 13:32:51 +0530

    initial commit

--- GeneralProgrammes/src/com/basic/HelloWorld.java
+++ GeneralProgrammes/src/com/basic/HelloWorld.java
@@ -3,6 +3,10 @@ package com.basic;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world...");
+System.out.println("Hello world...");
+System.out.println("Hello world...");
+System.out.println("Hello world...");
+
 	}
 
 }
